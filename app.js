2d2344aa4cd369124090ebabb8399af700be7816
Ulises Brutti <ulisesbrutti@gmail.com>
2025-03-12 17:04:55 -0300

    Actualización del archivo app.js a la v0.2

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarNombres () {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);    
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
 
-    if (document.getElementById('amigo').value == '') {
-        alert('Ingresar un nombre válido');
-    } else {
+function agregarAmigo() {
         
+    if (document.getElementById('amigo').value == '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(document.getElementById('amigo').value);
+        console.log(amigos);
+        alert('Amigo ingresado exitosamente')
     }
+    limpiarCaja()
+    return;
 }
