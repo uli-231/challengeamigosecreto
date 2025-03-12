01a9ed288bc21e2637537f019ae1a5bfd04fa1c8
Ulises Brutti <ulisesbrutti@gmail.com>
2025-03-12 15:46:01 -0300

    verificación de ingreso de nombre

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarNombres () {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);    
+
+    if (document.getElementById('amigo').value == '') {
+        alert('Ingresar un nombre válido');
+    } else {
+        
+    }
+}
